e77f6f18a40505978351ee11ee0e512d9502bfb7
MaxenceAll <maxence.allart@gmail.com>
2023-05-21 03:35:32 +0200

    refactor reset

--- controllers/v1/login/resetController.js
+++ controllers/v1/login/resetController.js
@@ -6,37 +6,38 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 async function resetPassword(req, res) {
-  consolelog("resetPassword route taken");
-  const { email } = req.body;
-  const sql = "SELECT * FROM account WHERE email = ?";
-  await query(sql, [email])
-    .then(async (json) => {
-      const user = json.length === 1 ? json.pop() : null;
-      if (user) {
-        const { id, email } = user;
-        const data = { id, email };
-        const token = jwt.sign(data, process.env.JWT_SECRET, { expiresIn: '1h' });
-        const html = `Pour renouveler votre mot de passe, cliquez sur ce lien <a href="${config.FRONTEND.URL}reset?t=${token}">Nouveau mot de passe</a>`;
-        const mailParams = {
-          to: email,
-          subject: "Nouveau mot de passe demandé pour PCCompare",
-          html: html,
-        };
-        const mailResult = await mailer.send(mailParams);
-        consolelog(mailResult);
-        res.json(mailResult);
-      } else {
-        return res.status(401).json({ data: null, result: false, message: "Erreur lors de la demande de nouveau mot de passe !"})
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).json({ data: null, result: false, message: "Erreur serveur interne." });
-    });
+  consolelog("// Appel de la method resetPassword de resetController //");
+  try {
+    const { email } = req.body;
+    const SQL_FIND_USER = "SELECT * FROM account WHERE email = ?";
+    const [user] = await query(SQL_FIND_USER, [email]);
+    // un empty array est considéré falsy:
+    if (user) {
+      const { id, email } = user;
+      const data = { id, email };
+      const token = jwt.sign(data, process.env.JWT_SECRET, { expiresIn: '1h' });
+      const html = `Pour renouveler votre mot de passe, cliquez sur ce lien <a href="${config.FRONTEND.URL}reset?t=${token}">Nouveau mot de passe</a>`;
+      const mailParams = {
+        to: email,
+        subject: "Nouveau mot de passe demandé pour PCCompare",
+        html: html,
+      };
+      const mailResult = await mailer.send(mailParams);
+      consolelog(`!! Retour du mailer :`,mailResult);
+      res.status(200).json({data: mailResult, result: true, message: `Mail de réinitialisation de mot passe envoyé avec succès.`});
+    } else {
+      consolelog(`XX Aucun compte avec l'email ${email} !`)
+      res.status(401).json({ data: null, result: false, message: "Erreur lors de la récupération de mot de passe." });
+    }
+  } catch (error) {
+    consolelog(`XX Erreur dans resetPassword :`,error)
+    res.status(500).json({ data: null, result: false, message: "Erreur lors de la récupération de mot de passe." });
+  }
 }
 
+
 async function newPassword(req, res) {
-    consolelog("newPassword route taken");
+  consolelog("// Appel de la method newPassword de resetController //");
     const { body } = req;
     try {
       if (!body.token) {
@@ -47,8 +48,7 @@ async function newPassword(req, res) {
         return res.status(401).json({ data: null, result: false, message: "Token invalide ou expiré !" });
       }
       const hash = bcrypt.hashSync(body.password1, 10);
-      const hashedPassword = hash.replace(config.hash.prefix, "");
-  
+      const hashedPassword = hash.replace(config.hash.prefix, "");  
       const sql = `
         UPDATE account
         SET password = ?,
@@ -59,17 +59,17 @@ async function newPassword(req, res) {
       try {
         const result = await query(sql, [hashedPassword, data.email]);
         if (result.affectedRows >0){
-          return res.status(200).json({ data: result, result: true, message: "Mot de passe changé !" });
+          return res.status(200).json({ data: result, result: true, message: "Mot de passe changé avec succès !" });
         } else {
-          return res.status(401).json({ data: null, result: false, message: "Mot de passe non changé :( !"})
+          return res.status(401).json({ data: null, result: false, message: "Erreur lors de la modification de mot de passe !"})
         }
       } catch (error) {
         console.error(error);
-        res.status(500).json({ data: null, result: false, message: "Erreur serveur interne. (code 500)" });
+        res.status(500).json({ data: null, result: false, message: "Erreur serveur interne." });
       }
     } catch (error) {
-      console.error(error);
-      res.status(401).json({ data: null, result: false, message: "Token invalide ou expiré ! (code 401)" });
+      consolelog(`XX Erreur dans newPassword :`,error)
+      res.status(500).json({ data: null, result: false, message: "Erreur lors de la modification de mot de passe !" });
     }
   }
   
